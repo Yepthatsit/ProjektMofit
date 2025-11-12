ca55b8c641824f5791137fe4b4bdb31317ea9c85
Nujron <lukasz.s.puchala@gmail.com>
2025-11-12 13:28:47 +0100

    praca

--- Utilis.py
+++ Utilis.py
@@ -1,6 +1,19 @@
 import pandas as pd
+import types
+import numpy as np
+from numba import njit
+import math
+import matplotlib.pyplot as plt
+from math import sin, cos, pi, e, exp
+from scipy.linalg import eigh
+
 def two():
     return 2
 
 def ReadFile(FilePath)-> pd.DataFrame:
     return pd.read_csv(FilePath,sep = r"\s+", header=None)
+
+def pos_real(grid: types.Iterable, k: int):
+    pos = np.zeros(2)
+    for i in range(4):
+        pos[0] += grid.iloc[k-1, 0]
