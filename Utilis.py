eccd70cb647c8421aaa68802769360ff3c297d45
Nujron <lukasz.s.puchala@gmail.com>
2025-11-18 13:19:59 +0100

    pozycje real xy

--- Utilis.py
+++ Utilis.py
@@ -17,18 +17,58 @@ def pos_real(grid: pd.DataFrame, k: int):
     pos = np.zeros(2)
     for i in range(4):
         pos[0] += grid.iloc[k-1, 0]
+
+
+# funkcje biliniowe
+@njit
+def f1(ksi):
+    return (1-ksi)/2
+@njit
+def f2(ksi):
+    return (1+ksi)/2
+@njit
+def gi(i:int,ksi1,ksi2):
+    assert i >0 and i <5
+    match i:
+        case 1:
+            return f1(ksi1)*f1(ksi2)
+        case 2:
+            return f2(ksi1)*f1(ksi2)
+        case 3:
+            return f1(ksi1)*f2(ksi2)
+        case 4:
+            return f2(ksi1)*f2(ksi2)
+@njit 
+def xy_nlg(k:int, i:int, nlg:np.ndarray, wezly:np.ndarray):
+    k_is = nlg[:,1][nlg[:,0] == k]
+    k_globals = nlg[:,2][nlg[:,0] == k]
+    k_i_global = k_globals[k_is == i]
+    global_number = k_i_global[0]
+    x_nlg = wezly[:,1][wezly[:,0] == global_number][0]
+    y_nlg = wezly[:,2][wezly[:,0] == global_number][0]
+    return x_nlg, y_nlg
+    
+def xryr(k:int, ksi1:int, ksi2:int, nlg:np.ndarray, wezly:np.ndarray):
+    # k - nr elementu
+    x, y = 0, 0
+    for i in range(1,4+1):  
+        x_nlg, y_nlg = xy_nlg(k, i, nlg, wezly)
+        x += x_nlg*gi(i, ksi1, ksi2)
+        y += y_nlg*gi(i, ksi1, ksi2)
+    return x, y
+    
+
+
+# funkcje bikwadratowe
 @njit
 def q1(ksi):
     return ksi*(ksi-1)/2
-
 @njit
 def q2(ksi):
     return (1-ksi)*(1-ksi)
-
 @njit
 def q3(ksi):
     return ksi*(ksi+1)/2
-
 @njit
 def hi(i:int,ksi1,ksi2):
     assert i >0 and i <10
